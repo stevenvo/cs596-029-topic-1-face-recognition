30d4703f4bc69726a6fe1da5705b7cf4a0096d6c
Steven Vo <findme@stevenvo.info>
2015-07-25 18:33:26 -0700

    add printing result comparison

--- src/recognize_faces.py
+++ src/recognize_faces.py
@@ -17,6 +17,7 @@ DEFAULT_FACE_SIZE = 120.0
 TARGET_NAMES = ['Tracy', 'Trish', 'Steven']
 TRAINING = 'training'
 TESTING = 'testing'
+N_COMPONENTS = 30
 
 def recognize_face():
 
@@ -43,28 +44,7 @@ def recognize_face():
                 y.append(int(id))
         return X, y
 
-    # return is an array of faces in reshape (1,-1) format
-    # def read_testing_cropped_faces():
-    #     X_test = []
-    #     y_test = []
-    #     for path in glob(TESTING_IMAGES_DIRECTORY):
-    #         print path
-    #         img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    #         # set_trace()
-    #         img = img.flatten()
-    #         X_test.append(img)
-    #
-    #         p = re.compile(ur'^.*\/(\d*)-(.*)-face')
-    #         search_obj = re.search(p, path)
-    #         id, pname = search_obj.group(1), search_obj.group(2)
-    #         y_test.append(int(id))
-    #         print np.array(X_test)
-    #     # set_trace()
-    #     return X_test, y_test
-
-    def compute_pca(X_train, y_train, n_components = 30):
-        # X_train = [[1, 2, 3], [4, 5, 6]]
-        # print len(X_train)
+    def compute_pca(X_train, y_train, n_components = N_COMPONENTS):
         pca = RandomizedPCA(n_components, whiten=True).fit(X_train)
         eigenfaces = pca.components_.reshape((n_components, DEFAULT_FACE_SIZE, DEFAULT_FACE_SIZE))
         return pca, eigenfaces
@@ -79,28 +59,24 @@ def recognize_face():
                       'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
         clf = GridSearchCV(SVC(kernel='rbf', class_weight='auto'), param_grid)
         clf = clf.fit(X_train_pca, y_train)
-        print("Best estimator found by grid search: {0}".format(clf.best_estimator_))
+        # print("Best estimator found by grid search: {0}".format(clf.best_estimator_))
         return clf
 
     def validate_model(clf, X_test_pca, y_test):
         y_pred = clf.predict(X_test_pca)
-        print(classification_report(y_test, y_pred, target_names=TARGET_NAMES))
         return y_pred
 
     def print_comparison_result(y_test, y_predict):
+        print "======CLASSIFICATION REPORT======="
+        print(classification_report(y_test, y_predict, target_names=TARGET_NAMES))
+        print "======TEST RESULT COMPARISON======="
         for i, y in enumerate(y_test):
-            print "{0}.\tPrediction = {1}\tPredict/Target: {2}/{3}".format(i,
+            print "{0}.\tPrediction = {1}\tActual/Predict: {2}/{3}".format(i,
                 "CORRECT" if y == y_predict[i] else "INCORRECT",
-                TARGET_NAMES[y_predict[i]-1], TARGET_NAMES[y-1])
-
-
+                TARGET_NAMES[y-1], TARGET_NAMES[y_predict[i]-1])
 
     X_train, y_train = read_cropped_faces(mode=TRAINING)
-    # print X_train
-    # print y_train
     X_test, y_test = read_cropped_faces(mode=TESTING)
-    # print X_test
-    # print y_test
 
     pca, eigenfaces = compute_pca(X_train, y_train)
     X_train_pca, X_test_pca = transform_X(pca, X_train, X_test)
